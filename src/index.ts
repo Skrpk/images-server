7a631a00c38a1c48e8393f9fe51dc2e8594df527
Skrpk <vitaliy.skripka23@gmail.com>
2021-03-02 21:11:01 +0200

    removed useless logic

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,6 @@ app.use(morgan('tiny'));
 
 app.use('/', router);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!!');
-});
-
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
